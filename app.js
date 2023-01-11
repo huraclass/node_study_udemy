33b6ff363b352436e3d1bac6ea5a42b8765ade0a
huraclass <85816950+huraclass@users.noreply.github.com>
2023-01-11 13:24:28 +0900

    버전2

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use('/',(request,response,next) => {
+    response.status(404).send('<h1>404 NOT FOUND</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
