92e0d9368527f91f36a79c585d6370f88850bf0c
huraclass <85816950+huraclass@users.noreply.github.com>
2023-01-11 14:35:20 +0900

    버전2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
